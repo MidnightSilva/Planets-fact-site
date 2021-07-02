b46941be19831d5acf3bdb2ac3020be3763b840a
Jorge <jorgesilva1892@gmail.com>
2021-07-02 13:53:09 -0700

    setting up folders for scss, components, and importing assests from figma files

---
+++ client/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Nav = (props) => {
+
+    return (
+        <div>
+            <h2>The Planets</h2>
+            <ul>
+                <li></li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Nav
