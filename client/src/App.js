888024696ea195c6c396b331c590edfb673be0fa
Jorge <jorgesilva1892@gmail.com>
2021-07-05 15:00:12 -0700

    added state to the nav to hide and reveal the nav links

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import './App.css';
+import './Scss/App.scss';
+import "./Scss/Nav.scss"
+import "./Scss/Variables.scss"
 import Nav from './components/Nav'
 function App() {
   return (
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,20 +1,66 @@
-import React from 'react'
-import data from '../data.json'
+import hamburgerMenuDisabled from '../assets/hamburgerMenuFaded.svg'
+import hamburgerMenu from "../assets/hamburgerMenu.svg";
 
+import React, { useState } from "react";
 const Nav = (props) => {
 
+// i was thinking of mapping through this array to show however i wasnt sure
+//how to do that and react router dom. for now i will do this way. 
+// i will come back to refactor my code 
 
+
+// const planets = [
+//   "Mercury",
+//   "Venus",
+//   "Earth",
+//   "Mars",
+//   "Jupiter",
+//   "Saturn",
+//   "Uranus",
+//   "Neptune"
+// ];
+const [hamburgermenu , setHamburgerMenu] = useState (true)
+
+const toggleHamburgerMenu = () => {
+  hamburgermenu ? setHamburgerMenu(false) : setHamburgerMenu(true); 
+}
 
     return (
-        <div>
-            <h2>The Planets</h2>
-           {
-data.map((data) =>
-<div>{data.name}</div> ) 
- }
-            
+      <nav>
+        <div className="nav-top">
+          <p className="nav-title">The Planets</p>
+          {hamburgermenu ? (
+            <img
+              src={hamburgerMenuDisabled}
+              alt="hamburgermenu-logo"
+              className="hamburgerMenu"
+              onClick={toggleHamburgerMenu}
+            />
+          ) : (
+            <img
+              src={hamburgerMenu}
+              alt="hamburgermenu-logo"
+              className="hamburgerMenu"
+              onClick={toggleHamburgerMenu}
+            />
+          )}
         </div>
-    )
+        {
+          hamburgermenu ?
+          <div className="nav-items">
+          <p className="items">Mercury</p>
+          <p className="items">Venus</p>
+          <p className="items">Earth</p>
+          <p className="items">Mars</p>
+          <p className="items">Jupiter</p>
+          <p className="items">Saturn</p>
+          <p className="items">Uranus</p>
+          <p className="items">Neptune</p>
+        </div> :
+        null
+        }
+      </nav>
+    );
 }
 
 export default Nav
