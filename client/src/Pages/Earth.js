9cd65f78c22ca3f2b84d3bc3dc22c8d98e35ec67
Jorge <jorgesilva1892@gmail.com>
2021-08-25 21:12:03 -0700

    adjusted layout for desktop, need to pass the update to the other pages as well as the btn fix, after that minior adjusts will be left. project almost done

--- client/src/Pages/Earth.js
+++ client/src/Pages/Earth.js
@@ -16,45 +16,47 @@ const Earth = (props) => {
   return (
     <div className="page-container">
       {/* // buttons could be made into a component and import in here twice  */}
-      <MobileButtons />
-      {(count === 1 && <PlanetImage className="planet" image={earth} />) ||
-        (count === 2 && (
-          <PlanetImage className="planet" image={earthInternal} />
-        )) ||
-        (count === 3 && (
-          <PlanetImage className="planet-surface" image={earthSurface} />
-        ))}
-      <div className="planet-des--btn-container">
-        <div className="planet-des-wiki-container">
-          {(count === 1 && (
-            <PlanetDes
-              title="Earth"
-              des="Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive."
-            />
+      <div className="Layout">
+        <MobileButtons />
+        {(count === 1 && <PlanetImage className="planet" image={earth} />) ||
+          (count === 2 && (
+            <PlanetImage className="planet" image={earthInternal} />
           )) ||
-            (count === 2 && (
+          (count === 3 && (
+            <PlanetImage className="planet-surface" image={earthSurface} />
+          ))}
+        <div className="planet-des--btn-container">
+          <div className="planet-des-wiki-container">
+            {(count === 1 && (
               <PlanetDes
                 title="Earth"
-                des="Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle."
+                des="Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive."
               />
             )) ||
-            (count === 3 && (
-              <PlanetDes
-                title="Earth"
-                des="The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors."
-              />
-            ))}
-          {(count === 1 && (
-            <PlanetLink href="https://en.wikipedia.org/wiki/Mercury_(planet)" />
-          )) ||
-            (count === 2 && (
-              <PlanetLink href="https://en.wikipedia.org/wiki/Mercury_(planet)#Internal_structure" />
+              (count === 2 && (
+                <PlanetDes
+                  title="Earth"
+                  des="Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle."
+                />
+              )) ||
+              (count === 3 && (
+                <PlanetDes
+                  title="Earth"
+                  des="The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors."
+                />
+              ))}
+            {(count === 1 && (
+              <PlanetLink href="https://en.wikipedia.org/wiki/Mercury_(planet)" />
             )) ||
-            (count ===  3 && (
-              <PlanetLink href="https://en.wikipedia.org/wiki/Mercury_(planet)#Surface_geology" />
-            ))}
+              (count === 2 && (
+                <PlanetLink href="https://en.wikipedia.org/wiki/Mercury_(planet)#Internal_structure" />
+              )) ||
+              (count === 3 && (
+                <PlanetLink href="https://en.wikipedia.org/wiki/Mercury_(planet)#Surface_geology" />
+              ))}
+          </div>
+          <Buttons />
         </div>
-        <Buttons />
       </div>
       <div className="planet-cards-container">
         <PlanetCards title="Rotation Time" des=".99 Days" />
